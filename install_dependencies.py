212327c90e9a63792ab7d1fc34d604035dbdb8f5
vedad26230504-cpu <veda.d.26230504@gmail.com>
2026-03-26 14:36:53 +0530

    Create install_dependencies.py

---
+++ install_dependencies.py
@@ -0,0 +1,5 @@
+!pip install -q beautifulsoup4 requests langchain openai chromadb sentence-transformers python-dotenv
+!pip install -q langchain-huggingface
+!pip install -U langchain langchain-community langchain-huggingface chromadb sentence-transformers
+!pip install faiss-cpu
+!apt-get -y install python3-lxml
