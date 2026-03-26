a87a1c79bcbed18632a7c159d8387ecff33a1477
vedad26230504-cpu <veda.d.26230504@gmail.com>
2026-03-26 14:38:41 +0530

    Create predefined_queries.py

---
+++ predefined_queries.py
@@ -0,0 +1,5 @@
+question = "Explain the step-by-step process of integrating the Capillary SDK on Android."
+response = chatbot.get_answer(question)
+
+print(" Answer:", response["answer"])
+print(" Sources:", response["sources"])
